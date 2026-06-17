6309575521509c0ea7d300810278c19168e0ed05
Kakuya Ando <fservak@gmail.com>
2026-06-17 17:34:12 +0900

    feat(zsh): skip space-prefixed commands from history
    Add hist_ignore_space so commands typed with a leading space are not
    recorded in ~/.zsh_history.

--- zsh/modules/history.zsh
+++ zsh/modules/history.zsh
@@ -3,5 +3,6 @@ HISTFILE=${HOME}/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
 setopt hist_ignore_all_dups # 直前のコマンドと同じ場合、履歴に追加しない
+setopt hist_ignore_space # 先頭にスペースを入れたコマンドは履歴に残さない
 setopt share_history # share command history data
 setopt extended_history # 履歴に日付も入れる
